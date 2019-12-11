f9676b51ec9502d63481411516b8543e6351b3c6
Martin Ashby <martin@ashbysoft.com>
2019-12-11 13:21:22 +0000

    Changed the network to use macvlan driver instead of bridge (the default) This seems to fix the issue (all hostname lookups succeed)

--- gen.py
+++ gen.py
@@ -4,12 +4,17 @@ if __name__=="__main__":
     n_instances = int(argv[1])
     print("""
 version: '3'
+networks:
+  app_net:
+    driver: macvlan
 services:
 """)
     for i in range(n_instances):
         print("""
   app-{i}:
     image: app
+    networks: 
+     - app_net
     environment: 
       - N_INSTANCES={n_instances}
 """.format(i=i, n_instances=n_instances))
